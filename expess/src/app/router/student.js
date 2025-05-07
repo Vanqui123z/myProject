72fa14c8d31a5f4bd5a81b669e81f102101cd084
Vanqui <92575737+Vanqui123z@users.noreply.github.com>
2025-05-07 13:18:32 +0700

    Delete expess directory

--- expess/src/app/router/student.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const InforStudentControllers = require('../controllers/InforStudentControllers');
-
-
-// Route con /news/hello
-router.get('/inf', InforStudentControllers.show);
-
-module.exports = router;
